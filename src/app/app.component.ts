86a5b132fc5d15fa8532dd6c06e62a938b6c07ea
andrea.mena <andrea.mena@exmerdev.com>
2022-02-07 12:44:34 -0500

    New version and notification of new version for user

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'angular-pwa-app';
+
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+          if(confirm("New version available. Do you want load New Version?")) {
+
+              window.location.reload();
+          }
+      });
+    }   
+  }
+
+}
